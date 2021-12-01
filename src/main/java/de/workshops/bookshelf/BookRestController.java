b2e67f15ba4cd212583e7abbf4d0f355e9d15802
Björn Wilmsmann <bjoern@wilmsmann.de>
2021-12-01 13:20:15 +0100

    Sample solutions from day 1

---
+++ src/main/java/de/workshops/bookshelf/BookRestController.java
@@ -0,0 +1,38 @@
+package de.workshops.bookshelf;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+
+@RestController
+@RequestMapping("/book")
+public class BookRestController {
+
+    private final ObjectMapper mapper;
+    private final ResourceLoader resourceLoader;
+
+    private List<Book> books;
+
+    public BookRestController(final ObjectMapper mapper, final ResourceLoader resourceLoader) {
+        this.mapper = mapper;
+        this.resourceLoader = resourceLoader;
+    }
+
+    @PostConstruct
+    public void init() throws Exception {
+        final var resource = resourceLoader.getResource("classpath:books.json");
+        this.books = mapper.readValue(resource.getInputStream(), new TypeReference<>() {});
+    }
+
+    @GetMapping
+    public List<Book> getAllBooks() {
+        return books;
+    }
+}
